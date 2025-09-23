afb0c7a48315ccd0f07cafa8f7fa8584d38cd094
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-09-23 21:46:54 +0530

    working tracker

--- cmd/client_example/tracker_client_example.go
+++ cmd/client_example/tracker_client_example.go
@@ -9,9 +9,6 @@ import (
 )
 
 func main() {
-	// Example: Using the tracker client
-
-	// 1. Parse a torrent file to get info hash and tracker URL
 	torrentData := []byte("d8:announce21:http://localhost:80804:infod6:lengthi1024e4:name8:test.txt12:piece lengthi32768e6:pieces20:12345678901234567890ee")
 
 	torrent, err := meta.ParseTorrent(torrentData)
@@ -28,53 +25,79 @@ func main() {
 	fmt.Printf("Tracker: %s\n", torrent.Announce)
 	fmt.Printf("Info Hash: %x\n", infoHash)
 
-	// 2. Create tracker client
-	peerID := "-PC0001-123456789012" // Your peer ID
-	port := 6881                     // Your listening port
+	fmt.Println("\nTesting multiple peers")
+	testMultiplePeers(torrent.Announce, infoHash, torrent.Info.Length)
+}
 
-	client := client.NewTrackerClient(peerID, port)
+func testMultiplePeers(trackerURL string, infoHash [20]byte, fileSize int64) {
+	peers := []struct {
+		id   string
+		port int
+		name string
+	}{
+		{"-PC0001-123456789012", 6881, "Peer A"},
+		{"-PC0002-abcdefghijkl", 6882, "Peer B"},
+		{"-PC0003-mnopqrstuvwx", 6883, "Peer C"},
+	}
 
-	// 3. Make announce requests
-	fmt.Println("\n=== ANNOUNCE: STARTED ===")
-	response, err := client.Announce(torrent.Announce, infoHash, torrent.Info.Length, "started")
-	if err != nil {
-		log.Printf("Announce failed: %v", err)
-	} else {
-		fmt.Printf("Interval: %d seconds\n", response.Interval)
-		fmt.Printf("Peers found: %d\n", len(response.Peers))
-		for i, peer := range response.Peers {
-			fmt.Printf("  Peer %d: %s:%d (ID: %s)\n", i+1, peer.IP, peer.Port, peer.PeerID)
+	clients := make([]*client.TrackerClient, len(peers))
+
+	fmt.Println("\nStep 1: All peers announce started")
+	for i, peer := range peers {
+		clients[i] = client.NewTrackerClient(peer.id, peer.port)
+		response, err := clients[i].Announce(trackerURL, infoHash, fileSize, "started")
+		if err != nil {
+			log.Printf("%s announce failed: %v", peer.name, err)
+		} else {
+			fmt.Printf("%s: Interval=%ds, Peers=%d\n", peer.name, response.Interval, len(response.Peers))
+			for j, p := range response.Peers {
+				fmt.Printf("  Peer %d: %s:%d (ID: %s)\n", j+1, p.IP, p.Port, p.PeerID)
+			}
 		}
 	}
 
-	// 4. Simulate download progress
-	client.UpdateStats(1024, 512) // uploaded 1KB, downloaded 512B
-
-	fmt.Println("\n=== ANNOUNCE: UPDATE ===")
-	response, err = client.Announce(torrent.Announce, infoHash, torrent.Info.Length-512, "")
+	fmt.Println("\nStep 2: Peer A downloads some data")
+	clients[0].UpdateStats(512, 256)
+	response, err := clients[0].Announce(trackerURL, infoHash, fileSize-256, "")
 	if err != nil {
-		log.Printf("Announce failed: %v", err)
+		log.Printf("Peer A update failed: %v", err)
 	} else {
-		fmt.Printf("Updated stats sent. Peers: %d\n", len(response.Peers))
+		fmt.Printf("Peer A: Updated stats, Peers=%d\n", len(response.Peers))
+		for i, p := range response.Peers {
+			fmt.Printf("  Peer %d: %s:%d (ID: %s)\n", i+1, p.IP, p.Port, p.PeerID)
+		}
 	}
 
-	// 5. Announce completion
-	client.UpdateStats(2048, torrent.Info.Length) // Download complete
-
-	fmt.Println("\n=== ANNOUNCE: COMPLETED ===")
-	response, err = client.Announce(torrent.Announce, infoHash, 0, "completed")
+	fmt.Println("\nStep 3: Peer B completes download")
+	clients[1].UpdateStats(1024, fileSize)
+	response, err = clients[1].Announce(trackerURL, infoHash, 0, "completed")
 	if err != nil {
-		log.Printf("Announce failed: %v", err)
+		log.Printf("Peer B completion failed: %v", err)
 	} else {
-		fmt.Printf("Download completed! Final peer count: %d\n", len(response.Peers))
+		fmt.Printf("Peer B: Download completed! Peers=%d\n", len(response.Peers))
+		for i, p := range response.Peers {
+			fmt.Printf("  Peer %d: %s:%d (ID: %s)\n", i+1, p.IP, p.Port, p.PeerID)
+		}
 	}
 
-	// 6. Stop announcing
-	fmt.Println("\n=== ANNOUNCE: STOPPED ===")
-	response, err = client.Announce(torrent.Announce, infoHash, 0, "stopped")
+	fmt.Println("\nStep 4: Peer C checks current peers")
+	response, err = clients[2].Announce(trackerURL, infoHash, fileSize, "")
 	if err != nil {
-		log.Printf("Announce failed: %v", err)
+		log.Printf("Peer C update failed: %v", err)
 	} else {
-		fmt.Printf("Stopped announcing.\n")
+		fmt.Printf("Peer C sees %d other peers:\n", len(response.Peers))
+		for i, p := range response.Peers {
+			fmt.Printf("  Peer %d: %s:%d (ID: %s)\n", i+1, p.IP, p.Port, p.PeerID)
+		}
+	}
+
+	fmt.Println("\nStep 5: All peers stop")
+	for i, peer := range peers {
+		_, err := clients[i].Announce(trackerURL, infoHash, 0, "stopped")
+		if err != nil {
+			log.Printf("%s stop failed: %v", peer.name, err)
+		} else {
+			fmt.Printf("%s: Stopped announcing\n", peer.name)
+		}
 	}
 }
--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,1 +1,89 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/pixperk/pixtorrent/tracker"
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	log.Println("=== PiXTorrent Tracker Server ===")
+
+	redisAddr := getEnvOrDefault("REDIS_ADDR", "localhost:6379")
+	redisPassword := getEnvOrDefault("REDIS_PASSWORD", "")
+	redisDB := 0
+	trackerAddr := getEnvOrDefault("TRACKER_ADDR", ":8080")
+
+	log.Printf("Connecting to Redis at %s (DB: %d)", redisAddr, redisDB)
+	log.Printf("Starting tracker server on %s", trackerAddr)
+
+	ctx := context.Background()
+	storage := tracker.NewRedisStorage(ctx, redisAddr, redisPassword, redisDB)
+
+	testClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPassword,
+		DB:       redisDB,
+	})
+	_, err := testClient.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+	testClient.Close()
+	log.Println("connected to Redis successfully")
+
+	trackerServer := tracker.NewTracker(trackerAddr, storage)
+
+	go func() {
+		log.Printf("Tracker server starting on %s", trackerAddr)
+		log.Println("Endpoints:")
+		log.Println("  - GET /announce - BitTorrent announce endpoint")
+		log.Println("  - GET /scrape   - BitTorrent scrape endpoint")
+
+		if err := trackerServer.Start(); err != nil {
+			log.Printf("Server error: %v", err)
+		}
+	}()
+
+	setupGracefulShutdown(trackerServer, storage)
+}
+
+func setupGracefulShutdown(trackerServer *tracker.Tracker, storage tracker.Storage) {
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	sig := <-sigChan
+	log.Printf("Received signal: %v", sig)
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := trackerServer.Server.Shutdown(ctx); err != nil {
+		log.Printf("Error during server shutdown: %v", err)
+	} else {
+		log.Println("HTTP server shut down")
+	}
+
+	if err := storage.Close(); err != nil {
+		log.Printf("Error closing storage: %v", err)
+	} else {
+		log.Println("Storage connections closed")
+	}
+
+	log.Println("Tracker server stopped")
+}
+
+func getEnvOrDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
