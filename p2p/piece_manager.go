b576318a3258aaf6b357b3f2aa002ed1a3250862
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-09-28 11:43:09 +0530

    piece manager

---
+++ p2p/piece_manager.go
@@ -0,0 +1,58 @@
+package p2p
+
+import (
+	"fmt"
+	"sync"
+)
+
+type PieceManager struct {
+	mu        sync.RWMutex
+	numPieces int
+	pieces    map[int][]byte //idx -> data
+}
+
+func NewPieceManager(numPieces int) *PieceManager {
+	return &PieceManager{
+		numPieces: numPieces,
+		pieces:    make(map[int][]byte),
+	}
+}
+
+func (pm *PieceManager) HavePiece(idx int) bool {
+	pm.mu.RLock()
+	defer pm.mu.RUnlock()
+
+	_, exists := pm.pieces[idx]
+	return exists
+}
+
+func (pm *PieceManager) AddPiece(idx int, data []byte) error {
+	pm.mu.Lock()
+	defer pm.mu.Unlock()
+
+	if idx < 0 || idx >= pm.numPieces {
+		return fmt.Errorf("piece index out of range")
+	}
+	if _, exists := pm.pieces[idx]; exists {
+		return fmt.Errorf("piece %d already exists", idx)
+	}
+	pm.pieces[idx] = data
+	return nil
+}
+
+func (pm *PieceManager) Bitfield() []byte {
+	pm.mu.RLock()
+	defer pm.mu.RUnlock()
+
+	size := (pm.numPieces + 7) / 8
+	bitfield := make([]byte, size)
+
+	for i := 0; i < pm.numPieces; i++ {
+		if _, ok := pm.pieces[i]; ok {
+			byteIndex := i / 8
+			bitIndex := 7 - (i % 8) // MSB first, like BitTorrent spec
+			bitfield[byteIndex] |= 1 << bitIndex
+		}
+	}
+	return bitfield
+}
